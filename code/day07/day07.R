3bf498e35ef649f825af1db9fcc46f996bedc56c
christianholland <cholland2408@gmail.com>
2023-12-07 21:38:12 +0100

    fixed bug

--- code/day07/day07.R
+++ code/day07/day07.R
@@ -5,6 +5,9 @@ library(tidyverse)
 camel_cards <- read_delim(here("code/day07/input"), delim = " ", col_names = c("hand", "bid")) |>
   rowid_to_column("id")
 
+# labels 
+types <- ordered(fct_rev(as_factor(c("Five of a kind", "Four of a kind", "Full house", "Three of a kind", "Two pair", "One pair", "High card"))))
+
 # helper function to convert a hand to its type
 hand2type <- function(hand, labels) {
   freq <- map_int(labels, \(x) str_count(hand, as.character(x)))
@@ -13,19 +16,19 @@ hand2type <- function(hand, labels) {
     as.integer()
 
   if (freq_code >= 50000) {
-    type <- "Five of a kind"
+    type <- types[1]
   } else if (freq_code >= 40000) {
-    type <- "Four of a kind"
+    type <- types[2]
   } else if (freq_code >= 32000) {
-    type <- "Full house"
+    type <- types[3]
   } else if (freq_code >= 30000) {
-    type <- "Three of a kind"
+    type <- types[4]
   } else if (freq_code >= 22000) {
-    type <- "Two pair"
+    type <- types[5]
   } else if (freq_code >= 20000) {
-    type <- "One pair"
+    type <- types[6]
   } else if (freq_code >= 10000) {
-    type <- "High card"
+    type <- types[7]
   }
   type
 }
@@ -36,7 +39,7 @@ camel_cards |>
   mutate(type = map(hand, \(x) hand2type(x, labels))) |>
   unnest(type) |>
   separate_wider_position(hand, widths = c(card1 = 1, card2 = 1, card3 = 1, card4 = 1, card5 = 1)) |>
-  mutate(across(where(is_character), \(x) ordered(x, levels = rev(labels)))) |>
+  mutate(across(where(is.character), \(x) ordered(x, levels = rev(labels)))) |>
   arrange(type, card1, card2, card3, card4, card5) |>
   mutate(rank = row_number()) |>
   mutate(prod = bid * rank) |>
@@ -53,7 +56,7 @@ hand2type_with_joker <- function(hand) {
 
     # replace J by the label that occurs the most often to make the hand the
     # strongest type possible. For ties take the strongest card
-    replacement <- max(labels2[which(freq == max(freq))])
+    replacement <- max(labels2[which(freq[-13] == max(freq[-13]))])
     hand <- str_replace_all(hand, "J", as.character(replacement))
   }
 
