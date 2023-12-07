8368b6833f74c4648de8c739ca8893a49ded6317
christianholland <cholland2408@gmail.com>
2023-12-07 21:20:09 +0100

    day 7

---
+++ code/day07/day07.R
@@ -0,0 +1,73 @@
+library(here)
+library(tidyverse)
+
+# load data
+camel_cards <- read_delim(here("code/day07/input"), delim = " ", col_names = c("hand", "bid")) |>
+  rowid_to_column("id")
+
+# helper function to convert a hand to its type
+hand2type <- function(hand, labels) {
+  freq <- map_int(labels, \(x) str_count(hand, as.character(x)))
+  freq_code <- rev(sort(freq))[1:5] |>
+    str_c(collapse = "") |>
+    as.integer()
+
+  if (freq_code >= 50000) {
+    type <- "Five of a kind"
+  } else if (freq_code >= 40000) {
+    type <- "Four of a kind"
+  } else if (freq_code >= 32000) {
+    type <- "Full house"
+  } else if (freq_code >= 30000) {
+    type <- "Three of a kind"
+  } else if (freq_code >= 22000) {
+    type <- "Two pair"
+  } else if (freq_code >= 20000) {
+    type <- "One pair"
+  } else if (freq_code >= 10000) {
+    type <- "High card"
+  }
+  type
+}
+
+# part 1
+labels <- ordered(fct_rev(as_factor(c("A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"))))
+camel_cards |>
+  mutate(type = map(hand, \(x) hand2type(x, labels))) |>
+  unnest(type) |>
+  separate_wider_position(hand, widths = c(card1 = 1, card2 = 1, card3 = 1, card4 = 1, card5 = 1)) |>
+  mutate(across(where(is_character), \(x) ordered(x, levels = rev(labels)))) |>
+  arrange(type, card1, card2, card3, card4, card5) |>
+  mutate(rank = row_number()) |>
+  mutate(prod = bid * rank) |>
+  pull(prod) |>
+  sum()
+
+# part 2
+# J is now a joker and is considered the weakest card
+labels2 <- ordered(fct_rev(as_factor(c("A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"))))
+
+hand2type_with_joker <- function(hand) {
+  if (str_detect(hand, "J")) {
+    freq <- map_int(labels2, \(x) str_count(hand, as.character(x)))
+
+    # replace J by the label that occurs the most often to make the hand the
+    # strongest type possible. For ties take the strongest card
+    replacement <- max(labels2[which(freq == max(freq))])
+    hand <- str_replace_all(hand, "J", as.character(replacement))
+  }
+
+  tibble(modified_hand = hand, type = hand2type(hand, labels2))
+}
+
+
+camel_cards |>
+  mutate(df = map(hand, hand2type_with_joker)) |>
+  unnest(df) |>
+  separate_wider_position(hand, widths = c(card1 = 1, card2 = 1, card3 = 1, card4 = 1, card5 = 1)) |>
+  mutate(across(where(is_character), \(x) ordered(x, levels = rev(labels2)))) |>
+  arrange(type, card1, card2, card3, card4, card5) |>
+  mutate(rank = row_number()) |>
+  mutate(prod = bid * rank) |>
+  pull(prod) |>
+  sum()
